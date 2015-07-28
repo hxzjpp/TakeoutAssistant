55d3b8a04d5cea089f0e0cfe09e838b4c9235d8b
hxzjpp <zhuangjing@gmail.com>
2015-07-28 15:58:58 -0400

    Initial Commit

---
+++ TakeoutAssistant_Team3_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TakeoutAssistant_Team3_ios
+//
+//  Created by Z J on 7/28/15.
+//  Copyright (c) 2015 Z J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TakeoutAssistant_Team3_ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TakeoutAssistant_Team3_ios
+//
+//  Created by Z J on 7/28/15.
+//  Copyright (c) 2015 Z J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
